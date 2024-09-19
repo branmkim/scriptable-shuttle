fdd13525f2d8df9e658721d826f007b918d39db5
branmkim <bran.mkim@gmail.com>
2024-09-19 11:46:19 -0400

    fixed time zones

--- app.js
+++ app.js
@@ -6,21 +6,22 @@ const monthu = require('./monthu.json')
 const fri = require('./fri.json')
 const weekend = require('./weekend.json')
 
+const weekdays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
 app.use(express.json());
 
 app.get('/api/times', (req, res) => {
-    const day = new Date().getDay();
-    const hour = new Date().getHours();
-    const min = new Date().getMinutes();
-
-    console.log(day, hour, min);
+    const now = new Date();
+    const weekday = weekdays.indexOf(now.toLocaleDateString("en-US", {timeZone: "America/New_York", weekday: "short"}))
+    const hour = now.toLocaleTimeString("en-US", {timeZone: "America/New_York", hour12: false, hour: "2-digit"})
+    const min = now.toLocaleTimeString("en-US", {timeZone: "America/New_York", hour12: false, minute: "2-digit"})
 
     let man = null;
     let mtc = null;
-    if (day >= 1 && day <= 4) {
+    if (weekday <= 3) {
         man = monthu.man;
         mtc = monthu.mtc;
-    } else if (day == 5) {
+    } else if (weekday == 4) {
         man = fri.man;
         mtc = fri.mtc;
     } else {
@@ -49,7 +50,7 @@ app.get('/api/times', (req, res) => {
     if (nextMtc + 1 < mtc.length - 1) resMtc.push(mtc[nextMtc + 1]);
     if (nextMtc + 2 < mtc.length - 1) resMtc.push(mtc[nextMtc + 2]);
 
-    res.json({ resMan, resMtc, day, hour, min });
+    res.json({ resMan, resMtc });
 });
 
 app.listen(port, () => {
